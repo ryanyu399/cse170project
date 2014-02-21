df26ff239fe2c5cd143b4835756c9c9bfb356336
Ryan Yu <ryyu@ucsd.edu>
2014-02-20 21:39:53 -0800

    finished json

--- routes/labs.js
+++ routes/labs.js
@@ -1,17 +1,19 @@
-
+// Get all of our rooms data
+var data = require('../data.json');
 
 exports.viewLabs = function(req, res) 
 {
-
   // controller code goes here
-  var name = req.params.name;
+  var name1 = req.params.name;
   var stank = req.params.stank;
   var crowd = req.params.crowd;
+  var desc = data.projects[0].description;
   
   res.render('labs', {
-    'room': name,
+    'room': name1,
     'stank' : stank,
-    'crowd' : crowd
+    'crowd' : crowd,
+    'description': desc
   });
 
 };
