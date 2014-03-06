a682bdf120038cbe6ba77e34dea74c9da684d734
Ryan Yu <ryyu@ucsd.edu>
2014-03-05 22:31:19 -0800

    fixed nav bar

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,6 @@
 // Get all of our rooms data
 var data = require('../data.json');
 
-exports.view = function(req, res){
-    var randomBoolean = Math.random() >= 0.5;
-
-    var modal = randomBoolean;
-    
-	res.render('index', {
-    'modal':modal
-  });
-};
 
 exports.view = function(req, res){
     var randomBoolean = Math.random() >= 0.5;
